04d2b441dbff5b738109b3d9e1eabe446cbb4168
AdolfoVillalobos <amvillalobos@uc.cl>
2019-07-13 17:45:18 -0400

    Update Problem 3
    Faster routine

--- problem3.py
+++ problem3.py
@@ -1,21 +1,13 @@
-from sympy.ntheory import isprime
-
 N = 600851475143
 
-
-def largest_prime(N):
-
-    current = N
-    list = []
-    for i in range(1, N):
-        if isprime(i):
-            if N%i==0:
-                list.append(i)
-                current /= i
-                if current == 1:
-                    break
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
         else:
-            continue
-    return max(list)
+            n //= i
+    return n
 
-print(largest_prime(2**(50)))
+%%time
+print(largest_prime_factor(N))
