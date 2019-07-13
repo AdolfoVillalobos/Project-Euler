d40ff0a50c81254151c8ca09618137a1c5fd0c6b
AdolfoVillalobos <amvillalobos@uc.cl>
2019-07-13 14:27:40 -0400

    Update Problem 1.py
    I came up with a faster implementation, using arithmetic formulas and set theory.

--- problem1.py
+++ problem1.py
@@ -1,5 +1,6 @@
-N = 1000
 
+
+## naive aproach
 def sum_divisors(N):
     list = []
     for t in range(1,N):
@@ -7,4 +8,24 @@ def sum_divisors(N):
             list.append(t)
     return sum(set(list))
 
-print(sum_divisors(1000))
+## Faster Aproach: Overlap
+def sum_divisors_fast(N):
+    a = obtain_max(3, N)
+    b = obtain_max(5, N)
+    c = N//(3*5)
+
+    return int(3*n_first(a)+5*n_first(b)-(3*5)*n_first(c))
+
+def obtain_max(p, n):
+    if n % p == 0:
+        return (n/p-1)
+    else:
+        return n//p
+
+def n_first(n):
+    return n*(n+1)/2
+%%time
+print(sum_divisors_fast(1000000))
+
+%%time
+print(sum_divisors(1000000))
